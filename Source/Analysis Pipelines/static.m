b6efd52bd49842a52774e7197f5ada9a0c740f08
Daniel Gordon <daniel.gordon@ed.ac.uk>
2019-01-15 17:52:26 +0000

    Distinct static script

---
+++ Source/Analysis Pipelines/static.m
@@ -0,0 +1,17 @@
+% This script performs automatic processing of static data collected
+% in the Gait lab at the University of Edinburgh.
+
+%% Parameters
+
+% Folder containing static .TRC data.
+static_folder = '';  
+
+% [x,y,z] rotations (in deg) required to get marker data in to OpenSim 
+% coord system.
+marker_rotations = {0,0,0}; 
+
+% Static save directory.
+save_static = '';
+
+%% Processing.
+processStaticData(static_folder, marker_rotations, save_static);
