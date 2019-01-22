c8776f714b8f59fd14931e51ffa3cea22dec5a56
Daniel Gordon <daniel.gordon@ed.ac.uk>
2019-01-22 11:38:34 +0000

    Bugfix

--- Source/File Processing/processGRFData.m
+++ Source/File Processing/processGRFData.m
@@ -5,7 +5,7 @@ grf_files = dir([data_folder filesep '*.mot']);
 n_files = length(grf_files);
 
 for i=1:n_files
-    input_grf = [input_folder filesep grf_files(i).name];
+    input_grf = [data_folder filesep grf_files(i).name];
     output_grf = produceMOT(input_grf, save_dir);
     grfs = Data(output_grf);
     grfs.rotate(rotations{:});
