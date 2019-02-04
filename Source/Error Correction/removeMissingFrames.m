5514c34117c85b7267678e678dd247d24d5de6a6
Daniel Gordon <DanielFNG@users.noreply.github.com>
2019-02-04 09:44:23 +0000

    Add removeMissingFrames function
    This function removes frames which feature missing data, either at the
    start or end of the data.
    
    It can work on a single data object or a combination of two.

--- Source/Error Correction/removeMissingFrames.m
+++ Source/Error Correction/removeMissingFrames.m
@@ -1,20 +1,30 @@
-function [markers, grfs] = renoveMissingFrames(marker_file, grfs)
-% Detect & remove missing timesteps in kinematics and (optionally) grfs.
+function removeMissingFrames(markers_file, grfs_file)
+% Detect & remove missing frames in a marker file, & optionally a grf file.
 
-try 
-    markers = Data(marker_file);
-catch err
-    if strcmp(err.identifier, 'Data:Gaps')
-        [start_markers, end_markers, markers] = ...
-            returnMissingFrames(marker_file);
-        
-        
-        frequency_multiplier = grfs.Frequency/
-        start_grfs = start_markers*
-    else
-        rethrow(err);
-    end
-end
+    % Use missingData function in TRCData class.
+    [gaps_start, gaps_end, markers] = TRCData.missingData(markers_file);
     
-
+    % Write resulting TRCData object to file.
+    markers.writeToFile(markers_file);
+    
+    % Deal with GRFs also. 
+    if nargin > 1
+    
+        % Identify the start/end of the problematic data - in terms of timesteps
+        % to account for data frequency mismatch.
+        marker_timesteps = markers.getColumn('time');
+        start_time = marker_timesteps(gaps_start+1);
+        end_time = marker_timesteps(gaps_end-1);
+        
+        % Load the grf file and isolate the non-problematic frames.
+        grfs = Data(grfs_file);
+        grf_timesteps = grfs.getColumn('time');
+        grf_frames = grf_timesteps(grf_timesteps >= start_time & ...
+            grf_timesteps <= end_time);
+            
+        % Slice only the good frames and print to file.
+        grfs.slice(grf_frames);
+        grfs.writeToFile(grfs_file);
+    end
+                
 end
