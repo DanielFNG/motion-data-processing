080728bb1d0577e6971558747b1920b6679abff3
Daniel Gordon <daniel.gordon@ed.ac.uk>
2019-03-10 22:54:06 +0000

    Allow separate frequencies for filtering moments and forces

--- Source/GRF Production/lp4FilterGRFs.m
+++ Source/GRF Production/lp4FilterGRFs.m
@@ -1,14 +1,14 @@
 function [forces, moments] = ...
-    lp4FilterGRFs(forces, moments, frequency, fp_frame_rate)
+    lp4FilterGRFs(forces, moments, force_freq, moments_freq, fp_frame_rate)
 
-    if nargin < 4
+    if nargin < 5
         fp_frame_rate = 600;
     end
     dt = 1/fp_frame_rate;
 
     forces(:, 1:6) = ...
-        ZeroLagButtFiltfilt(dt, frequency, 4, 'lp', forces(:, 1:6));
+        ZeroLagButtFiltfilt(dt, force_freq, 4, 'lp', forces(:, 1:6));
     moments(:, 1:6) = ...
-        ZeroLagButtFiltfilt(dt, frequency, 4, 'lp', moments(:, 1:6));
+        ZeroLagButtFiltfilt(dt, moments_freq, 4, 'lp', moments(:, 1:6));
 
 end
