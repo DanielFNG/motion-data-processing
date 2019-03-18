40558be59a35e5502338e191bc48b108de291e17
Daniel Gordon <daniel.gordon@ed.ac.uk>
2019-03-18 12:32:48 +0000

    Update compensateSpeedGRF.m

--- Source/Motion Compensation/compensateSpeedGRF.m
+++ Source/Motion Compensation/compensateSpeedGRF.m
@@ -1,4 +1,4 @@
-function grf_data = compensateSpeedGRF(grf_data, speed, direction, cutoff)
+function grf_data = compensateSpeedGRF(grf_data, speed, direction)
 % Compensate grfs for the speed of a fixed-speed reference frame.
 %
 % For example: motion on a treadmill.
@@ -18,20 +18,25 @@ function grf_data = compensateSpeedGRF(grf_data, speed, direction, cutoff)
     % correct direction, compensate for the provided fixed speed. 
     for i=1:grf_data.NCols
         if strcmpi(grf_data.Labels{i}(end-1:end), ['p' direction])
-            
-            % Account for reference frame speed.
-            initial_values = forces.getColumn(i);
+            initial_values = grf_data.getColumn(i);
             adjusted_values = accountForMovingReferenceFrame(...
                 initial_values, time, speed);
-            
-            % Re-set any CoP's to NaN if the vertical force is 0.
-            f_label = [grf_data.Labels{i}(1:end-2) 'vy'];
-            force_values = grf_data.getColumn(f_label);
-            adjusted_values(force_values < cutoff) = NaN;
-            
-            % Assign new column.
             grf_data.setColumn(i, adjusted_values);
+            
         end      
+    end
+    
+    % Re-set any CoP's to NaN if the vertical force is 0.
+    for i=1:grf_data.NCols
+        if strcmpi(grf_data.Labels{i}(end-1:end), 'vy')
+            force_values = grf_data.getColumn(i);
+            for direction = 'xyz'
+                label = [grf_data.Labels{i}(1:end-2) 'p' direction];
+                cop = grf_data.getColumn(label);
+                cop(force_values < 1e-8) = NaN;
+                grf_data.setColumn(label, cop);
+            end
+        end
     end
 
 end
