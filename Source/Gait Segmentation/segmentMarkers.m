ebe69c662f8f7e77de3d7d27e50f699a4bc6bdf8
Daniel Gordon <daniel.gordon@ed.ac.uk>
2019-03-24 13:27:37 +0000

    Implement more general marker segmentation
    Should work both with compensated speed & without - see 'Validation of motion data processing' note

--- Source/Gait Segmentation/segmentMarkers.m
+++ Source/Gait Segmentation/segmentMarkers.m
@@ -5,13 +5,39 @@ function [cycles_time, cycles_frame] = segmentMarkers(side, motion_data)
 % system (X forward from subject, Y up, Z to the right).    
 
     if strcmp(side, 'left')
-        str = 'L_MTP1_X';
+        upper_str = 'L_Thigh_Upper';
+        front_str = 'L_Thigh_Front';
     else
-        str = 'R_MTP1_X';
+        upper_str = 'R_Thigh_Upper';
+        front_str = 'R_Thigh_Front';
+    end
+    
+    % Calculate the angle between these markers. 
+    upper_x = motion_data.getColumn([upper_str '_X']);
+    upper_y = motion_data.getColumn([upper_str '_Y']);
+    front_x = motion_data.getColumn([front_str '_X']);
+    front_y = motion_data.getColumn([front_str '_Y']);
+    angle = atan((front_x - upper_x)./(upper_y - front_y));
+    
+    % Find the crossing points.
+    crossing_points = [];
+    for i=2:length(angle)
+        if angle(i) < 0 && angle(i - 1 ) >= 0
+            crossing_points = [crossing_points i];
+        end
+    end
+    
+    % Find the largest minimum peak between each crossing point.
+    indices = [];
+    for i=2:length(crossing_points)
+        temp = zeros(size(angle));
+        range = crossing_points(i - 1):crossing_points(i);
+        temp(range) = angle(range);
+        [~, idx] = min(temp);
+        indices = [indices idx];
     end
     
     % Note: motion_data must be in OpenSim coordinate system!
-    [~, indices] = findpeaks(motion_data.getColumn(str));
     n_cycles = length(indices) - 1;
     cycles_time = cell(1, n_cycles);
     cycles_frame = cycles_time;
