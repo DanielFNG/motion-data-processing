8c05a5a256f17ffeef4c1fc3b73940cd68c9e58e
Daniel Gordon <daniel.gordon@ed.ac.uk>
2019-04-09 18:14:35 +0100

    Improve CoP calculations
    We now take in to account the thickness of the belts on the treadmill as well as the metal plate on top the force plates themselves.

--- Source/GRF Production/calcCOP.m
+++ Source/GRF Production/calcCOP.m
@@ -1,13 +1,18 @@
 function cop = calcCOP(forces, moments)
 
+% Offset between plate and foot.
+h = 0.005;
+
 % Create array to store CoP data.
 cop = zeros(size(forces, 1), 6);
 
 % Perform CoP calculations.
 for i=0:1
     index = i*3; % Add 0 for right foot calcs, 3 for left.
-    cop(:, 1 + index) = -moments(:, 3 + index)./forces(:, 2 + index);
-    cop(:, 3 + index) = moments(:, 1 + index)./forces(:, 2 + index);
+    cop(:, 1 + index) = (h*forces(:, 1 + index) - moments(:, 3 + index))./ ...
+        forces(:, 2 + index);
+    cop(:, 3 + index) = (h*forces(:, 3 + index) + moments(:, 1 + index))./ ...
+        forces(:, 2 + index);
 end
 
 % Set NaN's to 0.
