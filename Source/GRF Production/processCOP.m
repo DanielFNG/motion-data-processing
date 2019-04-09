ca53429a13ea30db7b0bbca1563286739f7f9567
Daniel Gordon <daniel.gordon@ed.ac.uk>
2019-04-09 18:14:02 +0100

    Fix bug in processCOP
    This was causing the function to end early if only an untrusted portion of a gait cycle was collected as the first stance data.

--- Source/GRF Production/processCOP.m
+++ Source/GRF Production/processCOP.m
@@ -3,7 +3,10 @@ function [cop, forces, moments, time] = ...
 
     % Value of vertical force for which we trust the CoP calculation.
     f_min = 200;
-
+    
+    % Create an array which will store indices of data to be binned.
+    bin = [];
+    
     % For left and right feet.
     for k=0:3:3
         
@@ -36,11 +39,12 @@ function [cop, forces, moments, time] = ...
             % such that the CoP never becomes trustworthy (i.e. F_Y < f_min
             % for the entire portion) then... bin it, it's useless anyway.
             if isempty(valid_start_idx)
-                forces(range, :) = [];
-                moments(range, :) = [];
-                cop(range, :) = [];
-                time(range) = [];
-                break;
+                bin = [bin range];
+                
+                % Unless we're at the very beginning, we can stop now. 
+                if i ~= 1
+                    break;
+                end
             end
             
             valid_last_idx = find(forces(range, f_index) > f_min, 1, ...
@@ -50,11 +54,10 @@ function [cop, forces, moments, time] = ...
             % index since we made it here, just go to the end of the data
             % that we have.
             if isempty(valid_last_idx)
-                valid_last_idx = forces(end, f_index);
+                valid_last_idx = size(forces, 1);
             end
 
-           % Assume that CoP is unchanged outside of the valid range - but
-           % take in to account the relative speed.
+           % Assume that CoP is unchanged outside of the valid range.
            for ind = [cop_z_index, cop_x_index]
                cop(range(1):valid_start_idx - 1, ind) = ...
                    cop(valid_start_idx, ind);
@@ -64,5 +67,11 @@ function [cop, forces, moments, time] = ...
         end
 
     end
+    
+    % Bin the data the we don't trust and couldn't fix.
+    forces(bin, :) = [];
+    moments(bin, :) = [];
+    cop(bin, :) = [];
+    time(bin) = [];
 
 end
