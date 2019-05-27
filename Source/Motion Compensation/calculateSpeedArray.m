5d37827b5cf9a913f38293cdb8d6a0fe940223ba
Daniel Gordon <daniel.gordon@ed.ac.uk>
2019-05-27 16:41:02 +0100

    Update calculateSpeedArray.m
    Very messy way of accounting for two different formats of speed files. Eventually (quite a ways away) this should be fixed via the use of configuration XML files.

--- Source/Motion Compensation/calculateSpeedArray.m
+++ Source/Motion Compensation/calculateSpeedArray.m
@@ -3,7 +3,11 @@ function speed = calculateSpeedArray(speed_data, filter, threshold)
 % case. Currently assumes left speed = right speed.
 
     speed_data.filter4LP(filter);
-    speed = speed_data.getColumn('Right Speed');
+    speed = speed_data.getColumn('Speed');
+    % Uglily support two current forms of speed formatting. 
+    if size(speed, 2) == 0
+        speed = speed_data.getColumn('Right Speed');
+    end 
     speed(speed < threshold) = 0;
 
 end
