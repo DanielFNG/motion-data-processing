f0d45c6ef12259705942dee395043dccd7c26722
Daniel Gordon <daniel.gordon@ed.ac.uk>
2019-07-05 19:01:52 +0100

    Update readViconTextData.m
    Tidy up & remove hard-coded bugfixes.

--- Source/GRF Production/readViconTextData.m
+++ Source/GRF Production/readViconTextData.m
@@ -27,29 +27,10 @@ function [time, forces, moments] = readViconTextData(input_file)
     time = 0.001*(0:n_frames - 1)';
     forces(:, 1:3) = values(:, 3:5);
     forces(:, 4:6) = values(:, 12:14);
-    moments(:, 1:3) = 0.001*values(:, 6:8);  % Conversion from Nmm to Nm.
-    moments(:, 4:6) = 0.001*values(:, 15:17);
+    moments(:, 1:3) = values(:, 6:8);  % Conversion from Nmm to Nm.
+    moments(:, 4:6) = values(:, 15:17);
     
-    % Temporary - rotate the forces to get them like how they were before.
-    zrot = 0;
-    yrot = 180;
-    xrot = 90;
-    R = transpose(rotz(zrot)*roty(yrot)*rotx(xrot));
-    for i=0:3:3
-        forces(:, 1 + i:3 + i) = ...
-            transpose(R*transpose(forces(:, 1 + i:3 + i)));
-        moments(:, 1 + i:3 + i) = ...
-            transpose(R*transpose(moments(:, 1 + i:3 + i)));
-    end
-    
-    %% TEMPORARY BUGFIX!!
-%     forces(:, 1) = -forces(:, 1);
-     forces(:, 3) = -forces(:, 3);
-%     forces(:, 4) = -forces(:, 4);
-     forces(:, 6) = -forces(:, 6);
-%     moments(:, 1) = -moments(:, 1);
-     moments(:, 3) = -moments(:, 3);
-%     moments(:, 4) = -moments(:, 4);
-     moments(:, 6) = -moments(:, 6);
+    % Convert moments from Nmm to Nm. 
+    moments = 0.001*moments;
     
 end
