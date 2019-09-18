7a50b0b2861b3f02a12d629f64829a57c79dea28
Daniel Gordon <daniel.gordon@ed.ac.uk>
2019-09-18 18:05:57 +0100

    Improvements to findThresholdIndices
    These changes are to better deal with fringe cases:
    
    - We now threshold data at the beginning which was previously missed
    - Similar for the end, handling the case where we can drop below the upper boundary but never rise to it, and other such cases

--- Source/GRF Production/findThresholdIndices.m
+++ Source/GRF Production/findThresholdIndices.m
@@ -1,16 +1,26 @@
 function [left, right] = findThresholdIndices(forces, upper, lower)
 
 for side = [2, 5]
-    i = 2;
     vy = forces(:, side); 
-    indices = [];
+    i = find(vy > lower, 1, 'first') + 1;
+    indices = 1:(i - 2);
     while i < length(vy)
-        if vy(i-1) > upper && vy(i) <= upper
+        if vy(i - 1) > upper && vy(i) <= upper
             start = find(vy(i:end) < lower, 1, 'first');
-            mid = find(vy(i:end) > upper, 1, 'first');
-            last = find(vy(i:i + mid - 1) < lower, 1, 'last');
-            indices = [indices (i + start - 1):(i + last - 1)]; %#ok<*AGROW>
-            i = i + mid - 1;
+            if ~isempty(start)
+                mid = find(vy(i:end) > upper, 1, 'first');
+                if isempty(mid)
+                    last = find(vy(i:end) < lower, 1, 'last');
+                    indices = [indices (i + start - 1):(i + last - 1)];%#ok<*AGROW>
+                    i = length(vy);
+                else
+                    last = find(vy(i:i + mid - 1) < lower, 1, 'last');
+                    indices = [indices (i + start - 1):(i + last - 1)];
+                    i = i + mid - 1;
+                end
+            else
+                break;
+            end
         else
             i = i + 1;
         end
