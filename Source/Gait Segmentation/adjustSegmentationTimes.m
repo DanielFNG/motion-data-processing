bb6aad8adc2e329d12b470f986066e4b3aaf18ed
Daniel Gordon <daniel.gordon@ed.ac.uk>
2019-09-19 12:46:44 +0100

    Remove adjustSegmentationTimes
    Should not be the job of this function to do this. Indeed done earlier in motionDataProcessing. Don't lose much marker data.

--- Source/Gait Segmentation/adjustSegmentationTimes.m
+++ Source/Gait Segmentation/adjustSegmentationTimes.m
@@ -10,12 +10,8 @@ function [marker_frames, grf_frames] = ...
     for i=1:n_segments
         start = segmentation_times{i}(1);
         finish = segmentation_times{i}(end);
-        suitable_grf = grf_time(grf_time >= start & grf_time <= finish);
-        suitable_kin = kin_time(kin_time >= start & kin_time <= finish);
-        min_time = max(suitable_grf(1), suitable_kin(1));
-        max_time = min(suitable_grf(end), suitable_kin(end));
-        marker_frames{i} = kin_time >= min_time & kin_time <= max_time;
-        grf_frames{i} = grf_time >= min_time & grf_time <= max_time;
+        marker_frames{i} = kin_time >= start & kin_time <= finish;
+        grf_frames{i} = grf_time >= start & grf_time <= finish;
     end
 
 end
