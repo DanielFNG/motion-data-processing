c449f540b688f5c223b566e298a79380972c53e0
Daniel Gordon <daniel.gordon@ed.ac.uk>
2020-03-02 12:34:55 +0000

    Simplify overall processing procedure
    Simpler approach -> fewer steps

--- Source/GRF Production/produceMOT.m
+++ Source/GRF Production/produceMOT.m
@@ -1,50 +1,34 @@
 function grf_data = produceMOT(input_file, system, inclination, save_dir)
 
 % Get the arrays of time, forces and moments.
-[time, forces, moments, cop] = readViconTextData(input_file);
+[time, forces, moments, ~] = readViconTextData(input_file);
 
 % Convert quantities to OpenSim co-ordinates.
 forces(:, 1:3) = convertSystem(forces(:, 1:3), system);  % Left
 forces(:, 4:6) = convertSystem(forces(:, 4:6), system);  % Right
 moments(:, 1:3) = convertSystem(moments(:, 1:3), system);
 moments(:, 4:6) = convertSystem(moments(:, 4:6), system);
-cop(:, 1:3) = convertSystem(cop(:, 1:3), system);
-cop(:, 4:6) = convertSystem(cop(:, 4:6), system);
 
 % Gravity compensation.
 [forces, moments] = compensateGravity(forces, moments, inclination);
 
-% Initial threshold on the vertical force components. 
-forces(forces(:, 2) < 20, 1:3) = 0;
-forces(forces(:, 5) < 20, 4:6) = 0;
-
 % Apply initial low pass filters.
 [forces, moments] = lp4FilterGRFs(forces, moments, 10, 10);
 
-% Take absolute magnitude of vertical force components to remove negs.
-forces(forces(:, 2) < 0, 2) = -forces(forces(:, 2) < 0, 2);
-forces(forces(:, 5) < 0, 5) = -forces(forces(:, 5) < 0, 5);
-
-% Apply more low pass filtering.
-[forces, moments] = lp4FilterGRFs(forces, moments, 10, 10);
-
 % Threshold the force data.
-[left_indices, right_indices] = findThresholdIndices(forces, 40, 20);
-[forces, moments] = thresholdGRFs(...
-    forces, moments, left_indices, right_indices);
+[left_indices, right_indices] = findThresholdIndices(forces, 40, 2);
+forces(left_indices, 1:3) = 0;
+forces(right_indices, 4:6) = 0;
+moments(left_indices, 1:3) = 0;
+moments(right_indices, 4:6) = 0;
 
 % Process the raw cop data.
-cop = processRawCOP(cop, left_indices, right_indices);
-[cop_left, cop_right] = findThresholdIndices(forces, 450, 400);
-cop = adjustCOP(time, cop, left_indices, cop_left, right_indices, cop_right);
-cop(:, 1:6) = -cop(:, 1:6);  % Temporary hard coding weird thing...
-
-% Smoothing step. 
-% Smoothing for boundaries at low force values ( < 5-10N)
-%[forces, moments] = smoothGRFs(forces, moments);
+cop = calculateCOP(forces, moments, left_indices, right_indices);
+[cop_left, cop_right] = findThresholdIndices(forces, 110, 100);
+cop = adjustCOP(cop, left_indices, cop_left, right_indices, cop_right);
 
 % Calculate torques from free moments. 
-torques = calcTorques(forces, moments, cop);
+torques = calculateTorques(forces, moments, cop);
 
 % Construct overall data array. 
 data = constructGRFDataArray(forces, cop, torques);
