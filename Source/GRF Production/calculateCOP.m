1563c6f710faec6e3e01b7962b4033384a886cb0
Daniel Gordon <daniel.gordon@ed.ac.uk>
2020-03-02 12:33:37 +0000

    Update CoP computation
    Corrected calculations now closely match the data given by the treadmill, to within 1e-5, but crucially this now allows us to calculate on filtered rather than raw data.

--- Source/GRF Production/calculateCOP.m
+++ Source/GRF Production/calculateCOP.m
@@ -1,15 +1,27 @@
 function cop = calculateCOP(forces, moments, left_indices, right_indices)
 
-    z = 0.25; % height offset
+    % Initialise CoP array. 
     cop = zeros(size(forces));
+
+    % Treadmill true origin information.
+    x_left = -0.25;
+    x_right = 0.25;
+    y = 0.000001;
+    z = 0;
     
-    for i=[0,3]
-        cop(:, i + 1) = (1./forces(:, i + 2)).* ...
-            (moments(:, i + 3) - z*forces(:, i + 1));
-        cop(:, i + 3) = (1./forces(:, i + 2)).* ...
-            (moments(:, i + 1) - z*forces(:, i + 3));
-    end
+    % Compute moments at true treadmill origin.
+    true_x1 = -moments(:, 1) + y*forces(:, 3) + x_left*forces(:, 2); 
+    true_x2 = -moments(:, 4) + y*forces(:, 6) + x_right*forces(:, 5);
+    true_z1 = moments(:, 3) + z*forces(:, 2) - y*forces(:, 1);
+    true_z2 = moments(:, 6) + z*forces(:, 5) - y*forces(:, 4);
+        
+    % Compute CoP for both force plates.
+    cop(:, 1) = (1./forces(:, 2)).*(true_z1 + y*forces(:, 1));
+    cop(:, 3) = (1./forces(:, 2)).*(true_x1 + y*forces(:, 3));
+    cop(:, 4) = (1./forces(:, 5)).*(true_z2 + y*forces(:, 4));
+    cop(:, 6) = (1./forces(:, 5)).*(true_x2 + y*forces(:, 6));
     
+    % Assign 0 values to swing phase data. 
     cop(left_indices, 1:3) = 0;
     cop(right_indices, 4:6) = 0;
 
