4fd663e9b58c97821588d36b334b2c20098c6643
Daniel Gordon <daniel.gordon@ed.ac.uk>
2020-12-09 15:49:59 +0000

    Fix bug in createEMGData

--- Source/EMG/createEMGData.m
+++ Source/EMG/createEMGData.m
@@ -1,8 +1,8 @@
 function emg_data = createEMGData(time, data)
 
     labels{1} = 'time';
-    for i = 2:size(data, 2)
-        labels{i} = ['EMG ' num2str(i)];
+    for i = 1:size(data, 2)
+        labels{i + 1} = ['EMG ' num2str(i)];
     end
     
     values = [time, data];
